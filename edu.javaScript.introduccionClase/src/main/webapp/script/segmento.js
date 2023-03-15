7804dfa651fd0bd5c89f07229ace46b23a575769
fragalado <frandanigd@gmail.com>
2023-03-15 10:10:39 +0100

    15/03/2023

---
+++ edu.javaScript.introduccionClase/src/main/webapp/script/segmento.js
@@ -0,0 +1,26 @@
+/**
+ * CLASE SEGMENTO
+ */
+
+class Segmento {
+
+	// Constructor
+	constructor(inicio, final) {
+		this.inicio = inicio;
+		this.final = final;
+	}
+
+	// Métodos
+	distanciaPuntos() {
+		return this.final - this.inicio;
+	}
+}
+
+
+// Instanciamos la clase y calculamos la longitud de un segmento que tiene el punto inicial a los 5cm y el punto final a 20cm
+let sg1 = new Segmento(5, 20);
+
+// Imprimimos por la consola del navegador la longitud
+console.log(sg1.distanciaPuntos());
+console.log(typeof sg1);
+console.log(sg1 instanceof Segmento)
