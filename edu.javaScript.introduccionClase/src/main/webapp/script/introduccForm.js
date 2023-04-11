c58cf18bac467168c59f47d18bde1708d57cd6ce
fragalado <frandanigd@gmail.com>
2023-04-11 11:19:25 +0200

    11/04/2023

---
+++ edu.javaScript.introduccionClase/src/main/webapp/script/introduccForm.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+
+ 
+ function guardar(){
+	 let mensaje = document.getElementsByName('tuMensaje')[0].value;
+	 document.write("Se guarda el mensaje " + mensaje);
+ }
---
+++ edu.javaScript.introduccionClase/src/main/webapp/script/temporizadorVentana.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+function ventanaTemporal(){
+
+	
+	window.open("about::blank" , "ventana1", "width=120,height=300,scrollbars=NO");
+	
+	close("ventana1");
+}
